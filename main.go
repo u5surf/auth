918391afdb2918154519c89cecc7e4f3a342fd71
Adam Shannon <adamkshannon@gmail.com>
2018-09-26 16:28:53 -0500

    bump version for dev

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ import (
 	stdprometheus "github.com/prometheus/client_golang/prometheus"
 )
 
-const Version = "v0.2.0"
+const Version = "v0.3.0-dev"
 
 var (
 	httpAddr = flag.String("http.addr", ":8080", "HTTP listen address")
